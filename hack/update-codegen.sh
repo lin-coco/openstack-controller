d91d2b29dfcb7a74d0965059ac908f92097a6d43
薛业上 <2879322503@qq.com>
2023-11-21 17:27:19 +0800

    feat: 初始化搭建

---
+++ hack/update-codegen.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+source "${CODEGEN_PKG}/kube_codegen.sh"
+
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+
+kube::codegen::gen_helpers \
+    --input-pkg-root github.com/lincoco/sample-controller/pkg/apis \
+    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+
+kube::codegen::gen_client \
+    --with-watch \
+    --input-pkg-root github.com/lincoco/sample-controller/pkg/apis \
+    --output-pkg-root github.com/lincoco/sample-controller/pkg/generated \
+    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
---
+++ pkg/apis/samplecontroller/v1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+// +groupName=samplecontroller.k8s.io
+
+// Package v1 is the v1 version of the API.
+package v1 // import "github.com/lincoco/sample-controller/pkg/apis/samplecontroller/v1"
